7e0396f7280ed401a3787f83dd4d6a905eb45274
Rajeev Ranjan <65068998+rajeevranjan29@users.noreply.github.com>
2025-09-29 20:44:40 +0530

    Update settings.py

--- nexdrive/settings.py
+++ nexdrive/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [44.207.77.34]
 
 
 # Application definition
